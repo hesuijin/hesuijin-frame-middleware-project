d57547ead4c909679be63f2e29e3c01e064bb4b5
HSJ <hesuijin@qq.com>
2021-12-08 23:34:58 +0800

    中间件项目首次提交

---
+++ src/main/java/com/example/middleware/HesuijinMiddlewareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HesuijinMiddlewareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HesuijinMiddlewareProjectApplication.class, args);
+    }
+
+}
